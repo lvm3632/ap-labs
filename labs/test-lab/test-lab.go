892df40b26209b1d9bab9a8568e0cc3f129ecb39
Michel Lujano <a01636172@itesm.mx>
2021-08-17 19:09:45 +0000

    Solve test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	if len(os.Args) > 1 {
+		name := ""
+		for i := 1; i < len(os.Args); i++ {
+			name += os.Args[i] + " "
+		}
+		fmt.Printf("Welcome %v to the jungle \n", name)
+	} else {
+		fmt.Printf("The arguments are empty.\nPlease send a name or many.\n")
+		return
+	}
 }

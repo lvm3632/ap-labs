8c8c920df6bbd557efc74cdd58debec053feb209
Michel Lujano <a01636172@itesm.mx>
2021-09-08 00:38:00 +0000

    first partial

--- challenges/first-partial/c-month-day/month_day.c
+++ challenges/first-partial/c-month-day/month_day.c
@@ -1,8 +1,99 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
 /* month_day function's prototype*/
 void month_day(int year, int yearday, int *pmonth, int *pday);
 
-int main() {
+static char *name[] = {
+        "Illegal month",
+       "Jan", "Feb", "Mar",
+       "Apr", "May", "June",
+       "July", "Aug", "Sept",
+       "Oct", "Nov", "Dec"
+    };
+
+char *get_month(int *n){
+     return (*n < 1 || *n > 12) ? name[0] : name[*n];
+}
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, // 365: leap == 0   
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31} // 366: leap == 1
+
+};
+
+static int daysumtab[2][13] = {
+    {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365}, // 365: leap == 0   
+    {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366} // 366: leap == 1
+};
+
+int day_of_year(int year,int *month ,int day){
+    int i, leap;
+    if(year <= 0 || day <= 0 || day >= 367){
+        printf("Año ínvalido y/o día fuera del rango");
+        return 0;
+    }
+    // 1 or 0
+    leap = year%4 == 0 && year%100 != 0 || year % 400 == 0;
+    if(leap == 0 && day >= 366){
+        printf("El día es inválido, porque no es un año bisiesto\n");
+        return 0;
+    }
+ 
+    // Sacar month con el dia
+    if(day >= 1 && day <= 31){
+            *month = 1;
+     }else{
+         for(int j=1; j<12;j++){
+        // Intervalo de dias entre meses
+            if(day >= daysumtab[leap][j] && day <= daysumtab[leap][j+1]){
+                *month = ++j;
+                break;
+            } 
+        }
+     }
+
+    int lastday = day;
+    int n=0;
+    for(int m = daysumtab[leap][*month-1]+1; m <= lastday; m++){
+        n = n + 1;
+    }
+    
+    return n;
+}
+
+
+void month_day(int year, int yearday, int *pmonth, int *pday){
+
+    *pday = day_of_year(year,pmonth,yearday);
+    // En la función day_of_Year cambio el valor del apuntador de pmonth
+    // Aquí ya es el mes de acuerdo con el día
+    char *month = get_month(pmonth);
+
+}
+
+int main(int argc, char **argv) {
+
+    /* Input */
+    if(argc < 2){
+        printf("Please write at least two arguments. \nExample: \n");
+        printf("# ./month_day <year> <yearday> \n");
+        printf("# Example for Feb 2nd, 2019:\n");     
+        printf("./month_day 2019 33\n");
+        printf("Output: Feb 02, 2019\n");
+        return 0;
+    }
+
+    int year = atoi(argv[1]);
+    int yearday = atoi(argv[2]);
+    int pmonth = -1;
+    int pday = -1;
+
+    month_day(year, yearday, &pmonth, &pday);
+    if(pmonth != -1 && pday != -1){
+        printf("%s %02d, %d", name[pmonth], pday, year);
+    }
+    printf("\n");
+
     return 0;
 }
--- challenges/first-partial/go-functions-methods/geometry.go
+++ challenges/first-partial/go-functions-methods/geometry.go
@@ -7,18 +7,89 @@
 //!+point
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
 
-type Point struct{ X, Y float64 }
+type Point struct{ x, y float64 }
 
 // traditional function
 func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.y, q.y-p.y)
 }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.x, q.y-p.y)
+}
+
+func (p Point) X() float64 {
+	return p.x
+}
+
+func (p Point) Y() float64 {
+	return p.y
+}
+
+func onSegment(p, q, r Point) bool {
+	if q.X() <= math.Max(p.X(), r.X()) && q.X() >= math.Min(p.X(), r.X()) && q.Y() <= math.Max(p.Y(), r.Y()) && q.Y() >= math.Min(p.Y(), r.Y()) {
+		return true
+	}
+	return false
+}
+
+func orientation(p, q, r Point) int64 {
+	val := (q.Y()-p.Y())*(r.X()-q.X()) - (q.X()-p.X())*(r.Y()-q.Y())
+	if val == 0 {
+		return 0 //Colinear
+	}
+	if val > 0 {
+		return 1 // Clock
+	} else {
+		return 2 // Counterclock wise
+	}
+}
+
+func doIntersect(p1, q1, p2, q2 Point) bool {
+
+	// Casos especiales
+	o1 := orientation(p1, q1, p2)
+	o2 := orientation(p1, q1, q2)
+	o3 := orientation(p2, q2, p1)
+	o4 := orientation(p2, q2, q1)
+
+	// Caso general
+	if o1 != o2 && o3 != o4 {
+		return true
+	}
+
+	// Special Cases
+	// p1, q1 and p2 are colinear and p2 lies on segment p1q1
+	if o1 == 0 && onSegment(p1, p2, q1) {
+		return true
+	}
+	// p1, q1 and q2 are colinear and q2 lies on segment p1q1
+
+	if o2 == 0 && onSegment(p1, q2, q1) {
+		return true
+	}
+
+	// p2, q2 and p1 are colinear and p1 lies on segment p2q2
+	if o3 == 0 && onSegment(p2, p1, q2) {
+		return true
+	}
+
+	// p2, q2 and q1 are colinear and q1 lies on segment p2q2
+	if o4 == 0 && onSegment(p2, q1, q2) {
+		return true
+	}
+
+	return false
 }
 
 //!-point
@@ -31,15 +102,90 @@ type Path []Point
 // Distance returns the distance traveled along the path.
 func (path Path) Distance() float64 {
 	sum := 0.0
+	s := ""
 	for i := range path {
+		fmt.Printf("  - (%.2f,%.2f)", path[i].X(), path[i].Y())
+		fmt.Println()
 		if i > 0 {
-			sum += path[i-1].Distance(path[i])
+			dist := path[i-1].Distance(path[i])
+			sum += dist
+			s += fmt.Sprintf("%.2f + ", dist)
 		}
 	}
+	ultimo := path[0].Distance(path[len(path)-1])
+	sum += ultimo
+	s += fmt.Sprintf("%.2f", ultimo)
+	fmt.Printf("- Figure's Perimeter\n")
+	fmt.Printf("  - %s", s)
+	fmt.Printf(" = %.2f", sum)
+	fmt.Println()
 	return sum
 }
 
 func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Printf("Please, write at least one argument\n")
+		fmt.Println("Example: ")
+		fmt.Println("# go run geometry.go <number_of_sides>")
+		return
+	}
+	sides, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("You must write an Integer")
+		fmt.Println("Example: ")
+		fmt.Println("# go run geometry.go <number_of_sides>")
+		return
+	}
+	if sides <= 2 {
+		fmt.Printf("Please, provide a number greater or equal than three\n")
+		return
+	}
+
+	path := createIrregularFigure(sides)
+	path.Distance()
 }
 
-//!-path
+func createIrregularFigure(points int) Path {
+	// Creamos array de points
+	path := make(Path, points)
+	sides := len(path)
+
+	fmt.Printf("- Generating a [%d] sides figure\n", sides)
+	fmt.Printf("- Figure's vertices")
+	fmt.Println()
+
+	if sides > 3 { // Sí el polígono es mayor a 3 puntos
+		for i := 0; i < sides; i++ {
+			path[i] = getPoint() // Obtenemos un punto con valores aleatorios
+			if i >= 4 {          // Cuando tenemos al menos 2 líneas
+				p1 := path[i-3]                          // Eje: X - Linea 1
+				q1 := path[i-2]                          // Eje: Y - Linea 1
+				p2 := path[i-1]                          // Eje: X - Linea 2
+				q2 := path[i]                            // Eje: Y - Linea 2
+				intersect := doIntersect(p1, q1, p2, q2) // Sí ambas líneas se interceptan entonces es true.
+				for intersect {                          // Sí la línea sigue interceptando, entonces seguimos calculando con un punto distinto cada vez.
+					p2 = getPoint()
+					path[i-1] = p2
+					intersect = doIntersect(p1, q1, p2, q2)
+				}
+			}
+		}
+	} else {
+		for i := 0; i < sides; i++ {
+			path[i] = getPoint() // Sí el polígono tiene 3 vertices
+		}
+	}
+
+	return path
+}
+
+func generateRandom(min float64, max float64) float64 {
+	rand.Seed(time.Now().UnixNano())
+	return min + rand.Float64()*(max-min)
+}
+
+func getPoint() Point {
+	x, y := generateRandom(-100, 100), generateRandom(-100, 100)
+	return Point{x, y}
+}

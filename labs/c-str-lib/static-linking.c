522ad65f76e14152b491df479179aba90a10d56b
Michel Lujano <a01636172@itesm.mx>
2021-09-30 16:20:56 +0000

    Lab - String Manipulation Library

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+    if(argc <=  3){
+        printf("You need to send at least 3 arguments \nExample add:");
+        printf("./main.o -add 'Initial String ' 'This is the rest to be added'\n");
+        printf("Example find: ");
+        printf("./main.o -find 'Initial String ' 'Init'\n");
+        return -1;
+    }
+    char *operator_function = argv[1]; // -add or -find
+    char *palabra_original = argv[2];  // Origin word
+    char *segunda_palabra = argv[3];  // Append or find
+    if(strcmp(operator_function,"-add")==0){
+        char *palabra_final = mystradd(palabra_original,segunda_palabra);
+        printf("Initial Lenght      : %d\n",mystrlen(palabra_original));
+        printf("New String          : %s\n",palabra_final);
+        printf("New length          : %d\n",mystrlen(palabra_final));
+    }else if(strcmp(operator_function, "-find")==0){
+       int position = mystrfind(palabra_original, segunda_palabra);
+       if(position != -1){
+            printf("['%s'] string was found at [%d] position\n", segunda_palabra, position);
+       }else{
+           return -1;
+       }
+    }else{
+        printf("\nFunción inválida, solo se permiten las funciones -add y -find\n");
+        return -1;
+    }
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 
 int mystrlen(char *str){
-    return 0;
+    char *word = str;
+    int count = 0;
+    while(word[count] != '\0'){
+        count++;
+    }
+    return count;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int len_origin = mystrlen(origin);
+    int len_addition = mystrlen(addition);
+
+    int size = len_origin+len_addition;
+
+    char *palabra_final;
+    palabra_final = malloc(sizeof(int)*size);
+
+    for(int i=0;i<len_origin;i++){
+        palabra_final[i] = origin[i];
+    }
+
+    for(int i=0;i<len_addition;i++){
+        palabra_final[len_origin + i] = addition[i];
+    }
+
+    return palabra_final;
 }
 
 int mystrfind(char *origin, char *substr){
+
+    int len_origin = mystrlen(origin);
+    int len_substr = mystrlen(substr);
+
+    char *word_origin = origin;
+    char *word_substr = substr;
+
+    int count = 0;
+    int found = 0;
+    if(len_substr > len_origin){
+        printf("\n['%s'] string was not found at any position\n", substr);
+        return -1;
+    }else{
+        for(int i=0;i<len_origin;i++){
+            if(word_origin[i] == ' ')count++;
+            if(word_origin[i] != ' '){
+                if(word_origin[i] == word_substr[0]){
+                    int j=0;
+                    int k = i;
+                    while(k < len_origin && word_origin[k] == word_substr[j]){
+                            j++;
+                            k++; 
+                    }
+                    if(j >= len_substr){
+                        k = k - i;
+                    }else{
+                        j = -1;
+                        k = -2;
+                    }
+                    if(k == j){
+                        if(k == 1 && j == 1){
+                            if(word_origin[i]==word_substr[j-1]){
+                                 found = 1;
+                                 break;
+                            }
+                        }else{
+                             found = 1;
+                             break;
+                        }
+                    }
+                }
+            }
+        }
+        if(found == 1){
+                 //printf("['%s'] string was found at [%d] position", word_substr, count);
+            return count;
+        }else{
+                printf("\n['%s'] string was not found at any position\n", substr);
+                printf("Count: %d", count);
+                printf("\n");
+                return -1;
+        }
+    }
     return 0;
 }
